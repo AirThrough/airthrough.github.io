867e6d80903ff86056536295a61a9a1b980e9a7f
AirThrough <andrey.sckor@yandex.ru>
2018-05-06 09:44:28 +0300

    dsa

--- imperia/js/app.js
+++ imperia/js/app.js
@@ -14,7 +14,16 @@ $(document).ready(function(){
 	$(".popular-slider").owlCarousel({
 		items: 5, 
 		loop: true,
-		nav: true
+		nav: true,
+		responsive: {
+			0: {
+				items: 1,
+			},
+
+			1200: {
+				items: 5, 
+			}
+		}
 	});
 
 	$("select").niceSelect();
@@ -24,6 +33,16 @@ $(document).ready(function(){
 		loop: true,
 		nav: true,
 		dots: true
+	});
+
+
+	// MOBILE 
+
+	$(".open-menu-wrap").click(function(){
+
+		$(this).toggleClass('active');
+		$(".header-mobile").toggleClass('opened');
+
 	});
 
 
